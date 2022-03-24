a0fcafdd5fb0aa1d087d63670df065cf1378b6e4
sudhanshu <sudhanshukumarb6@gmail.com>
2022-03-24 14:23:39 +0530

    Initial commit

---
+++ New folder/App.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	a = 55
+
+	var b int = 500
+	var c = 100
+
+	d := 1100
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+}
